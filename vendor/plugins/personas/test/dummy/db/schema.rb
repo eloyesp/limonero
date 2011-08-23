874bca91f9a39cd6d1a2ecc81be7a5089e2c80aa
Eloy Espinaco <eloyesp@gmail.com>
2011-08-23 14:41:28 -0300

    ADD: personas plugin

--- vendor/plugins/personas/test/dummy/db/schema.rb
+++ vendor/plugins/personas/test/dummy/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,35 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110823174312) do
-
-  create_table "avaluos", :force => true do |t|
-    t.integer  "ncuotas"
-    t.integer  "cuota_cents"
-    t.integer  "cuota_inicial"
-    t.string   "cuota_currency", :limit => 3
-    t.float    "interes"
-    t.integer  "lote_id"
-    t.text     "observaciones"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "contratos", :force => true do |t|
-    t.date     "inicio"
-    t.integer  "avaluo_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "lotes", :force => true do |t|
-    t.string   "manzana"
-    t.string   "loteo"
-    t.string   "parcela"
-    t.string   "partida"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20110808134316) do
 
   create_table "personas_addresses", :force => true do |t|
     t.string   "street"
