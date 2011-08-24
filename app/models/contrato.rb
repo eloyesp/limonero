d9bba3f58fecca354c32e495d589fea78ceafd25
Eloy Espinaco <eloyesp@gmail.com>
2011-08-23 23:33:28 -0300

    FIX: Los modelos de Comprador y Contrato

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -1,5 +1,8 @@
 class Contrato < ActiveRecord::Base
   belongs_to :avaluo
   has_many :compradores
-  has_many :personas, :through => :compradores
+  has_many :persons,
+           :through => :compradores,
+           :class_name => "Personas::Person",
+           :source => :person
 end
--- spec/models/contrato_spec.rb
+++ spec/models/contrato_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Contrato do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:avaluo) }
+  it { should have_many(:compradores) }
+  it { should have_many(:persons) }
 end
